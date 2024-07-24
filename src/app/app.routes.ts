00ea5e324e910e4afdd8b6b22e707018fbfe063f
Matheus de Souza Badia <matheusbadia23@gmail.com>
2024-07-23 22:45:38 -0300

    header and footer created, used in the BasePageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginPageComponent} from "./components/pages/login-page/login-page.component";
+import {HomePageComponent} from "./components/pages/home-page/home-page.component";
+import {TestingComponent} from "./components/pages/testing/testing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "home", component: HomePageComponent },
+  { path: "login", component: LoginPageComponent },
+  { path: "test", component: TestingComponent },
+];
