9f839277ffb68fd9f14ece362878b778a696e7c7
Matheus de Souza Badia <matheusbadia23@gmail.com>
2024-08-06 12:57:29 -0300

    real-time email verification created

--- src/app/components/pages/register-page/register-page.component.ts
+++ src/app/components/pages/register-page/register-page.component.ts
@@ -1,4 +1,4 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BasePageComponent} from "../../itens/base-page/base-page.component";
 import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {RouterPagesService} from "../../../services/router-pages.service";
@@ -7,6 +7,7 @@ import {RegisterDTO} from "../../../interfaces/RegisterDTO";
 import {RegisterService} from "../../../services/register.service";
 import {HttpHeaders} from "@angular/common/http";
 import {LocalStorageService} from "../../../services/local-storage.service";
+import {debounceTime} from "rxjs";
 
 @Component({
   selector: 'app-register-page',
@@ -21,12 +22,16 @@ import {LocalStorageService} from "../../../services/local-storage.service";
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent implements OnInit {
+  protected readonly emailControlName: string = "emailInput"
+  protected readonly nameControlName: string = "nameInput"
+  protected readonly usernameControlName: string = "usernameInput"
+  protected readonly passwordControlName: string = "passwordInput"
+  protected readonly password2ControlName: string = "password2Input"
 
   public mainForm!: FormGroup;
 
   protected isEmailAvailable: boolean = false;
   protected isUsernameAvailable: boolean = false;
-
 
   private passwordValidatorPattern: string = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{8,}";
   private noSpaceValidatorPattern: string = "^[^\\s]+$"
@@ -37,8 +42,9 @@ export class RegisterPageComponent implements OnInit {
               private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.setRegisterFormGroup()
-    this.setRegisterFormControls()
+    this.setRegisterFormGroup();
+    this.setRegisterFormControls();
+    this.setEmailAvailabilityListener();
   }
 
   private setRegisterFormGroup() {
@@ -46,31 +52,33 @@ export class RegisterPageComponent implements OnInit {
   }
 
   private setRegisterFormControls() {
-    this.mainForm.addControl("nameInput", new FormControl( '', [Validators.required]))
+    this.mainForm.addControl(this.nameControlName, new FormControl( '', [
+      Validators.required,
+      Validators.minLength(2)]))
 
-    this.mainForm.addControl("usernameInput", new FormControl( '', [
+    this.mainForm.addControl(this.usernameControlName, new FormControl( '', [
       Validators.required,
       Validators.minLength(4),
       Validators.pattern(this.noSpaceValidatorPattern)]))
 
-    this.mainForm.addControl("emailInput", new FormControl('', [
+    this.mainForm.addControl(this.emailControlName, new FormControl('', [
       Validators.required,
       Validators.email
     ]))
 
-    this.mainForm.addControl("passwordInput", new FormControl('', [
+    this.mainForm.addControl(this.passwordControlName, new FormControl('', [
       Validators.required,
       Validators.pattern(this.passwordValidatorPattern)]))
 
-    this.mainForm.addControl("password2Input", new FormControl('', [
+    this.mainForm.addControl(this.password2ControlName, new FormControl('', [
       Validators.required,
       Validators.pattern(this.passwordValidatorPattern)]))
   }
 
   registerUser() {
-    let formValues: RegisterDTO = this.getRegisterFormValues();
-    if (this.mainForm.valid && this.bothPasswordsTheSame && this.isCredentialsAvailable()) {
-      this.registerService.registerUser(formValues)
+    let registerDTO: RegisterDTO = this.getRegisterFormValues();
+    if (this.isTheMainFormValid && this.bothPasswordsTheSame) {
+      this.registerService.registerUser(registerDTO)
         .subscribe(response => {
           const headers: HttpHeaders = response.headers;
           const jwtToken = headers.get("Authorization");
@@ -86,101 +94,130 @@ export class RegisterPageComponent implements OnInit {
 
   private getRegisterFormValues() {
     return {
-      name: this.mainForm.controls["nameInput"].value,
-      username: this.mainForm.controls["usernameInput"].value,
-      email: this.mainForm.controls["emailInput"].value,
-      password: this.mainForm.controls["passwordInput"].value
+      name: this.getNameControl.value,
+      username: this.getUsernameControl.value,
+      email: this.getEmailControl.value,
+      password: this.getPasswordControl.value
     }
   }
 
-  private isCredentialsAvailable() {
-    return this.isEmailAvailable && this.isUsernameAvailable;
+  private setEmailAvailabilityListener() {
+    this.getEmailControl.valueChanges
+      .pipe(
+        debounceTime(350))
+      .subscribe({
+        next: () => {
+          if (this.isEmailValid) {
+            this.checkEmailAvailability()
+          }
+        },
+      })
   }
 
-  /* EMAIL VALIDATIONS CONDITIONS */
-  get emailInvalidTouched() {
-    return this.mainForm.controls["emailInput"].invalid &&
-           this.mainForm.controls["emailInput"].touched;
+  private checkEmailAvailability() {
+    let email = this.getEmailControl.value
+    this.registerService.isEmailAvailable(email).subscribe({
+      next: (response) => {
+        this.isEmailAvailable = <boolean>response.body;
+      }
+    })
   }
 
-  get emailValid() {
-    return this.mainForm.controls["emailInput"].valid;
+  get isTheMainFormValid() {
+    return this.mainForm.valid
+  }
+
+  /* EMAIL VALIDATIONS CONDITIONS*/
+  get isEmailValid() {
+    return this.getEmailControl.status == "VALID"
+  }
+
+  get isEmailTouched() {
+    return this.getEmailControl.touched
   }
 
   /* NAME AND USERNAME VALIDATIONS CONDITIONS */
   get nameValid() {
-    return this.mainForm.controls["nameInput"].valid
+    return this.getNameControl.valid
   }
 
   get nameInvalidTouched() {
-    return this.mainForm.controls["nameInput"].valid &&
-      this.mainForm.controls["nameInput"].touched;
+    return !this.getNameControl.valid && this.getNameControl.touched;
   }
 
   get usernameValid() {
-    return this.mainForm.controls["usernameInput"].valid
+    return this.getUsernameControl.valid
   }
 
   get usernameInvalidTouched() {
-    return this.mainForm.controls["usernameInput"].invalid &&
-      this.mainForm.controls["usernameInput"].touched;
-  }
-
-  private setUsernameAvailableValue() {
-    let username = this.mainForm.controls["usernameInput"].getRawValue()
-    let isUsernameAvailable: boolean = false;
-    this.registerService.isUsernameAvailable(username).subscribe({
-      next: response => {
-        isUsernameAvailable = <boolean>response.body
-      }
-    })
-    this.isUsernameAvailable = isUsernameAvailable;
+    return this.getUsernameControl.invalid && this.getUsernameControl.touched;
   }
 
   /* PASSWORD VALIDATIONS CONDITIONS */
   get passwordValid() {
-    return this.mainForm.controls["passwordInput"].valid && this.mainForm.controls["password2Input"].valid
+    return this.getPasswordControl.valid && this.getPassword2Control.valid
   }
 
   get passwordInvalidTouched() {
-    let firstPasswordBooleanControl: boolean = this.mainForm.controls["passwordInput"].invalid &&
-      this.mainForm.controls["passwordInput"].touched;
+    let firstPasswordBooleanControl: boolean = this.getPasswordControl.invalid &&
+      this.getPasswordControl.touched;
 
-    let secondPasswordBooleanControl: boolean = this.mainForm.controls["password2Input"].invalid &&
-      this.mainForm.controls["password2Input"].touched;
+    let secondPasswordBooleanControl: boolean = this.getPassword2Control.invalid &&
+      this.getPassword2Control.touched;
 
     return firstPasswordBooleanControl && secondPasswordBooleanControl
   }
 
   get bothPasswordsTheSame(): boolean {
-    let password1 = this.mainForm.controls["passwordInput"].value
-    let password2 = this.mainForm.controls["password2Input"].value
+    let password1 = this.getPasswordControl.value
+    let password2 = this.getPassword2Control.value
     return password1 === password2
   }
 
   get passwordHasLeastOneDigit(): boolean {
-    const password = this.mainForm.controls["passwordInput"].value;
+    const password = this.getPasswordControl.value;
     return /\d/.test(password);
   }
 
   get passwordHasLeastOneLowerCase(): boolean {
-    const password = this.mainForm.controls["passwordInput"].value;
+    const password = this.getPasswordControl.value;
     return /[a-z]/.test(password);
   }
 
   get passwordHasLeastOneUpperCase(): boolean {
-    const password = this.mainForm.controls["passwordInput"].value;
+    const password = this.getPasswordControl.value;
     return /[A-Z]/.test(password);
   }
 
   get passwordHasMinLength(): boolean {
-    const password = this.mainForm.controls["passwordInput"].value;
+    const password = this.getPasswordControl.value;
     return password.length >= 8;
   }
 
   get passwordHasOnlyAllowedSpecialChars(): boolean {
-    const password = this.mainForm.controls["passwordInput"].value;
+    const password = this.getPasswordControl.value;
     return /^[a-zA-Z0-9$*&@#]*$/.test(password);
+  }
+
+  /* GET CONTROLS */
+  get getEmailControl() {
+    return this.mainForm.controls[this.emailControlName]
+  }
+
+  get getNameControl() {
+    return this.mainForm.controls[this.nameControlName]
+  }
+
+  get getUsernameControl() {
+    return this.mainForm.controls[this.usernameControlName]
+  }
+
+  get getPasswordControl() {
+    return this.mainForm.controls[this.passwordControlName]
+  }
+
+  get getPassword2Control() {
+    return this.mainForm.controls[this.password2ControlName]
   }
 
 }
